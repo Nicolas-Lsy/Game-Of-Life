9058e28dfd5522a8dc84b2c2b44dcf40ff2c47c5
pywizard <1670006261@qq.com>
2016-11-26 02:07:55 +0800

    这是乱码版本 生命游戏

---
+++ mess_game.py
@@ -0,0 +1,43 @@
+
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX=r'''
+               jnqpsu!sboepn"m>\\
+             sboepn.dipjdf((
+           0,2))!gp
+                s!j                      !jo
+         !sb                        ohf
+                (59)                      ^!gp
+         s!j!                      jo!s
+              bohf  (               5   9)^"
+         l>\\  0!g           ps!  j!jo!
+         sbohf  (59)^!   gps!j!   jo!sb
+         ohf(59  )^"jn   qpsu!   qzhbnf
+         "qzhbn   f.jo   ju()"   t>qzhb
+         nf.ejt                  qmbz.t
+         fu`npe       f((5        90,59
+         0),0,43)"    p>Usv  f"efg!{(y,
+         z);"!n>0"    !gps!  j!jo!(y-2,
+          y,y+2);"    !!gps   !k!jo!(z
+           -2, z   ,        z +2);"!!
+           !jg! j > > y ! b o e !z>>k
+  ;"!!!!d   poujo v f"!!!jg!(j= 0!ps !j?58!ps!
+k=0!ps!k? 58);"!!!!dp oujovf" !!!jg!(m\j ^\k^);
+"!! !  !n+>2"!sfuv s o ! n " jnqpsu!dpqz"xijmf!
+p;"   !t.gjmm((36              6,366,36   6))"!
+gps    !f!jo!qzhb              nf.fwfo    u.hfu
+();    "!!jg!f.uz              qf>>23    ;"!!!p
+>Gbmtf" !gps!y!jo!y   s      bohf(59     );"!!g
+ps!z!jo!ysbo   hf(59);"!!!n>{(y   ,!z)"!!!jg!n>
+  >3;"!!!!l\      y^\z^>m\y^\     z^"!!!fmjg!
+     n>>4;"!!!      !l\y^\z     ^>2"!!!f
+         mtf;"!!!!l\y^\z     ^>0"!!!
+      jg!l\y^\z^;"!!!     !t.gjmm((0,0,3
+66),(z*20,y*20,20,     20))"!!!!qzhbnf.esbx.sfd
+u(t ,(0,0,0),(       z*20,y*2    0,20,20),2)"!m
+>d                pqz.effqdpqz    (l  )"     !q
+zhb  nf   .ej t qmbz.vqebuf()"!q z h  b      nf
+.ujn  f    .xbju(200)"''';  
+exec(''.join(map(lambda x:chr(ord(x)-1)if ord(x) > 49 else x, reduce(
+	lambda x, i :  x.replace( chr(i), "\n "[34-i:35-i] ), range(35),
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX))))
+
+
